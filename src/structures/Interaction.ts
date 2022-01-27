7270b0e6754a413451bf2367ab4325fd5e318dc2
MARCROCK22 <marcos22dev@gmail.com>
2022-01-27 14:24:22 -0400

    editOrReply && sending

--- src/structures/Interaction.ts
+++ src/structures/Interaction.ts
@@ -3,12 +3,15 @@ import type { InteractionReplyData } from '../typings';
 import { parseMessage } from '../util/parsers';
 import { BaseInteraction, Embed } from '.';
 import FormData from 'form-data';
+import { FastifyReply } from 'fastify';
 
 export class Interaction extends BaseInteraction {
+    sending?: FastifyReply;
+
     public async rawReply(data: APIInteractionResponse, files: { filename: string; value: Buffer; }[] = []): Promise<void> {
         if (!data || Array.isArray(data) || typeof data !== 'object') throw new TypeError('data must be an object');
         if (!this.res) throw new Error('Reply called on an interaction that has no response.');
-        if (this.res.sent) throw new Error('Reply already sent.');
+        if (this.res.sent || this.sending) throw new Error('Reply already sent.');
         let body, headers: { ['Content-Type']?: string; } = {};
         if (Array.isArray(files) && files.length > 0) {
             body = new FormData();
@@ -25,9 +28,17 @@ export class Interaction extends BaseInteraction {
             headers['Content-Type'] = 'application/json';
         }
 
-        return await this.res
+        this.sending = this.res
             .headers(headers)
             .send(body);
+
+        return this.sending.then(() => {
+            delete this.sending;
+        }, (err) => {
+            delete this.sending;
+            throw err;
+        });
+
     }
 
     public reply(data: InteractionReplyData): Promise<void> {
@@ -48,7 +59,8 @@ export class Interaction extends BaseInteraction {
         }, files);
     }
 
-    public editReply(data: InteractionReplyData): Promise<APIMessage> {
+    public async editReply(data: InteractionReplyData): Promise<APIMessage> {
+        if (this.sending) await this.sending;
         if (!this.res.sent) throw new Error('Interaction can not be edited.');
         return this.client.rest.interaction.editOriginalInteractionResponse(this.applicationId, this.token, {
             ...data,
@@ -56,17 +68,26 @@ export class Interaction extends BaseInteraction {
         });
     }
 
-    public deleteReply(): Promise<void> {
+    public async editOrReply(data: InteractionReplyData): Promise<void | APIMessage> {
+        if (this.sending) await this.sending;
+        if (this.res.sent) return this.editReply(data);
+        return this.reply(data);
+    }
+
+    public async deleteReply(): Promise<void> {
+        if (this.sending) await this.sending;
         if (!this.res.sent) throw new Error('Interaction can not be deleted.');
         return this.client.rest.interaction.deleteOriginalInteractionResponse(this.applicationId, this.token);
     }
 
-    public fetchReply(): Promise<APIMessage> {
+    public async fetchReply(): Promise<APIMessage> {
+        if (this.sending) await this.sending;
         if (!this.res.sent) throw new Error('Interaction can not be fetched.');
         return this.client.rest.interaction.getOriginalInteractionResponse(this.applicationId, this.token);
     }
 
-    public defer(): Promise<void> {
+    public async defer(): Promise<void> {
+        if (this.sending) await this.sending;
         if (this.deferred) throw new Error('Interaction already deferred.');
         this.deferred = true;
         return this.rawReply({
